9c19d86ccce2ca8c88bfe0412d6d6e75b09667c4
Shreyas <skraman1999@gmail.com>
2014-09-18 17:05:08 -0700

    Added raw buttons.

--- src/robot/control/XboxController.java
+++ src/robot/control/XboxController.java
@@ -11,6 +11,17 @@ public class XboxController {
     public static final AxisType RIGHT_X = AxisType.kThrottle;
     public static final AxisType RIGHT_Y = AxisType.kNumAxis;
     
+    public static final int A = 1;
+    public static final int B = 2;
+    public static final int X = 3;
+    public static final int Y = 4;
+    public static final int LB = 5;
+    public static final int RB = 6;
+    public static final int BACK = 7;
+    public static final int START = 8;
+    public static final int L3 = 9;
+    public static final int R3 = 10;
+    
     private Joystick joystick;
     
     public XboxController() {
@@ -19,5 +30,11 @@ public class XboxController {
     
     public double getAxis(AxisType axis) {
         return joystick.getAxis(axis);
+    }
+    
+    public boolean getButton(int button) {
+        if (button<1 || button>10)
+            return false;
+        return joystick.getRawButton(button);
     }
 }
