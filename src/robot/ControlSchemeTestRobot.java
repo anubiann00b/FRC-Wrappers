672b21c109f9bad33bb806422b30c03e24113d77
Shreyas <skraman1999@gmail.com>
2014-09-18 17:17:05 -0700

    Set it up to jump right in.

--- src/robot/ControlSchemeTestRobot.java
+++ src/robot/ControlSchemeTestRobot.java
@@ -1,26 +1,26 @@
 package robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import robot.control.XboxController;
 import robot.drive.RobotDrive;
 import robot.drive.WoodchuckDrive;
 
 /**
  * A test of various control schemes.
  * 
- * @author [your name here]
+ * @author [name here]
  */
 public class ControlSchemeTestRobot extends IterativeRobot {
     
     RobotDrive drive;
+    XboxController controller;
     
     public void robotInit() {
         drive = new WoodchuckDrive();
+        controller = new XboxController();
     }
     
     public void teleopPeriodic() {
-        
+        // Write your drive code here.
     }
-    
-    // public void autonomousPeriodic() { }
-    // public void testPeriodic() { }
 }
